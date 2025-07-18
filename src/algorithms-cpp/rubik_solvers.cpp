59ad06a8c92fe61ea9951e7dbf794dc11e6a6d27
Shubh Gupta <142342135+titanShubh@users.noreply.github.com>
2025-07-19 04:10:47 +0530

    Update rubik_solvers.cpp

--- src/algorithms-cpp/rubik_solvers.cpp
+++ src/algorithms-cpp/rubik_solvers.cpp
@@ -1,173 +1,188 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <array>
 using namespace std;
 
-typedef vector<vector<vector<char>>> CubeState;
-vector<string> MOVES = {"R","R'","R2","L","L'","L2","U","U'","U2","D","D'","D2","F","F'","F2","B","B'","B2"};
+struct Cube {
+    array<int, 8> cp;  // Corner permutation
+    array<int, 8> co;  // Corner orientation
+    array<int, 12> ep; // Edge permutation
+    array<int, 12> eo; // Edge orientation
 
-class RubiksCube {
-private:
-    CubeState cube;
-    void rotateFaceClockwise(int f) {
-        auto& face = cube[f];
-        auto t = face;
-        for(int i=0;i<3;i++) for(int j=0;j<3;j++) face[j][2-i] = t[i][j];
-    }
-    void rotateFaceCounter(int f) {
-        rotateFaceClockwise(f);
-        rotateFaceClockwise(f);
-        rotateFaceClockwise(f);
-    }
-
-public:
-    RubiksCube() {
-        cube.resize(6, vector<vector<char>>(3, vector<char>(3)));
-        string cols = "wrgyob";
-        for(int f=0;f<6;f++) for(int i=0;i<3;i++) for(int j=0;j<3;j++) cube[f][i][j] = cols[f];
-    }
-
-    void applyRightMove() {
-        rotateFaceClockwise(1);
-        vector<char> t={cube[0][0][2],cube[0][1][2],cube[0][2][2]};
-        for(int i=0;i<3;i++) cube[0][i][2] = cube[2][i][2];
-        for(int i=0;i<3;i++) cube[2][i][2] = cube[3][i][2];
-        cube[3][0][2]=cube[5][2][0]; cube[3][1][2]=cube[5][1][0]; cube[3][2][2]=cube[5][0][0];
-        cube[5][2][0]=t[0]; cube[5][1][0]=t[1]; cube[5][0][0]=t[2];
-    }
-    void applyLeftMove() { rotateFaceClockwise(4); }
-    void applyUpMove() { rotateFaceClockwise(0); }
-    void applyDownMove() { rotateFaceClockwise(3); }
-    void applyFrontMove() { rotateFaceClockwise(2); }
-    void applyBackMove() { rotateFaceClockwise(5); }
-
-    void applyMove(const string& m) {
-        if(m=="R") applyRightMove();
-        else if(m=="R'") applyRightMove(),applyRightMove(),applyRightMove();
-        else if(m=="R2") applyRightMove(),applyRightMove();
-        else if(m=="L") applyLeftMove();
-        else if(m=="L'") applyLeftMove(),applyLeftMove(),applyLeftMove();
-        else if(m=="L2") applyLeftMove(),applyLeftMove();
-        else if(m=="U") applyUpMove();
-        else if(m=="U'") applyUpMove(),applyUpMove(),applyUpMove();
-        else if(m=="U2") applyUpMove(),applyUpMove();
-        else if(m=="D") applyDownMove();
-        else if(m=="D'") applyDownMove(),applyDownMove(),applyDownMove();
-        else if(m=="D2") applyDownMove(),applyDownMove();
-        else if(m=="F") applyFrontMove();
-        else if(m=="F'") applyFrontMove(),applyFrontMove(),applyFrontMove();
-        else if(m=="F2") applyFrontMove(),applyFrontMove();
-        else if(m=="B") applyBackMove();
-        else if(m=="B'") applyBackMove(),applyBackMove(),applyBackMove();
-        else if(m=="B2") applyBackMove(),applyBackMove();
+    Cube() {
+        for (int i = 0; i < 8; ++i) cp[i] = i, co[i] = 0;
+        for (int i = 0; i < 12; ++i) ep[i] = i, eo[i] = 0;
     }
 
     bool isSolved() const {
-        for(int f=0;f<6;f++){
-            char c=cube[f][0][0];
-            for(int i=0;i<3;i++) for(int j=0;j<3;j++) if(cube[f][i][j]!=c) return false;
-        }
+        for (int i = 0; i < 8; ++i)
+            if (cp[i] != i || co[i] != 0) return false;
+        for (int i = 0; i < 12; ++i)
+            if (ep[i] != i || eo[i] != 0) return false;
         return true;
     }
 
-    string toString() const {
-        string s;
-        for(auto& face: cube) for(auto& row: face) for(char c: row) s+=c;
-        return s;
+    string key() const {
+        string k;
+        for (int x : cp) k += to_string(x);
+        for (int x : co) k += to_string(x);
+        for (int x : ep) k += to_string(x);
+        for (int x : eo) k += to_string(x);
+        return k;
     }
 
-    int heuristic() const {
-        int cnt=0;
-        for(int f=0;f<6;f++){
-            char goal = cube[f][1][1];
-            for(int i=0;i<3;i++) for(int j=0;j<3;j++) if(cube[f][i][j]!=goal) cnt++;
-        }
-        return (cnt+7)/8;
+    bool operator==(const Cube& other) const {
+        return cp == other.cp && co == other.co && ep == other.ep && eo == other.eo;
     }
 };
 
-class IDAStar {
-private:
-    int nodes;
-    pair<bool, int> dfs(RubiksCube cube, vector<string>& path, int g, int thres, unordered_set<string>& vis) {
+// Basic hash function for unordered_set/map of Cube
+namespace std {
+    template <>
+    struct hash<Cube> {
+        size_t operator()(const Cube& c) const {
+            return hash<string>()(c.key());
+        }
+    };
+}
+
+// Move table
+unordered_map<string, Cube> moveTable;
+
+// Helper to compose two cube states
+Cube apply(const Cube& a, const Cube& move) {
+    Cube res;
+    for (int i = 0; i < 8; ++i) {
+        res.cp[i] = a.cp[move.cp[i]];
+        res.co[i] = (a.co[move.cp[i]] + move.co[i]) % 3;
+    }
+    for (int i = 0; i < 12; ++i) {
+        res.ep[i] = a.ep[move.ep[i]];
+        res.eo[i] = (a.eo[move.ep[i]] + move.eo[i]) % 2;
+    }
+    return res;
+}
+
+// Move definitions: U, D, L, R, F, B
+void initMoves() {
+    vector<string> faces = {"U", "D", "L", "R", "F", "B"};
+
+    // Base moves from Korf's model
+    unordered_map<string, Cube> base;
+    Cube m;
+
+    // U
+    m.cp = {3, 0, 1, 2, 4, 5, 6, 7};
+    m.co = {0, 0, 0, 0, 0, 0, 0, 0};
+    m.ep = {3, 0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11};
+    m.eo = {0};
+    base["U"] = m;
+
+    // D
+    m.cp = {0, 1, 2, 3, 5, 6, 7, 4};
+    m.ep = {0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 8};
+    base["D"] = m;
+
+    // L
+    m.cp = {4, 1, 2, 0, 7, 5, 6, 3};
+    m.co = {1,0,0,2,2,0,0,1};
+    base["L"] = m;
+
+    // R
+    m.cp = {0, 2, 6, 3, 4, 1, 5, 7};
+    m.co = {0,1,2,0,0,2,1,0};
+    base["R"] = m;
+
+    // F
+    m.cp = {0,1,3,7,4,5,2,6};
+    m.co = {0,0,1,2,0,0,2,1};
+    base["F"] = m;
+
+    // B
+    m.cp = {1,5,6,3,0,4,2,7};
+    m.co = {2,1,0,0,1,2,0,0};
+    base["B"] = m;
+
+    // Generate all 18 moves
+    for (auto& [face, m] : base) {
+        Cube x = m;
+        for (int i = 1; i <= 3; ++i) {
+            string name = face + (i == 1 ? "" : (i == 2 ? "2" : "'"));
+            moveTable[name] = x;
+            x = apply(x, m); // Repeated applications
+        }
+    }
+}
+
+// Simple heuristic (number of misplaced corners/edges)
+int heuristic(const Cube& c) {
+    int h = 0;
+    for (int i = 0; i < 8; ++i)
+        if (c.cp[i] != i || c.co[i] != 0) h++;
+    for (int i = 0; i < 12; ++i)
+        if (c.ep[i] != i || c.eo[i] != 0) h++;
+    return h / 8;  // Scaled down to keep f-cost tight
+}
+
+// IDA* search
+bool dfs(Cube& node, int g, int bound, vector<string>& path, string lastMove, Cube& goal, int& nodes) {
+    int f = g + heuristic(node);
+    if (f > bound) return false;
+    if (node == goal) return true;
+
+    for (auto& [name, move] : moveTable) {
+        if (!lastMove.empty() && name[0] == lastMove[0]) continue;
+        Cube child = apply(node, move);
+        path.push_back(name);
         nodes++;
-        string key = cube.toString();
-        if(vis.count(key)) return {false, INT_MAX};
-        int h = cube.heuristic();
-        int f = g + h;
-        if(f > thres) return {false, f};
-        if(cube.isSolved()) return {true, thres};
-        vis.insert(key);
-        int minT = INT_MAX;
-        for(auto& move : MOVES) {
-            RubiksCube next = cube;
-            next.applyMove(move);
-            path.push_back(move);
-            auto res = dfs(next, path, g+1, thres, vis);
-            if(res.first) return res;
-            minT = min(minT, res.second);
-            path.pop_back();
+        if (dfs(child, g + 1, bound, path, name, goal, nodes))
+            return true;
+        path.pop_back();
+    }
+    return false;
+}
+
+vector<string> idaStar(Cube start) {
+    Cube goal;
+    int bound = heuristic(start);
+    vector<string> path;
+    int nodes = 0;
+
+    while (true) {
+        if (dfs(start, 0, bound, path, "", goal, nodes)) {
+            cout << "Solved in " << path.size() << " moves, expanded " << nodes << " nodes.\n";
+            return path;
         }
-        vis.erase(key);
-        return {false, minT};
+        bound++;
+        if (bound > 20) break; // Limit depth
     }
+    return {};
+}
 
-public:
-    vector<string> solve(RubiksCube cube, int maxD = 12) {
-        nodes = 0;
-        int thres = cube.heuristic();
-        vector<string> path;
-        while(thres <= maxD) {
-            unordered_set<string> vis;
-            auto res = dfs(cube, path, 0, thres, vis);
-            if(res.first) {
-                cout << "IDA* Solution found!\nMoves: " << path.size() << "\nNodes: " << nodes << endl;
-                return path;
-            }
-            thres = res.second;
-            path.clear();
-        }
-        cout << "IDA* No solution found\n";
-        return {};
-    }
-};
-
-class KociembaAlgorithm {
-public:
-    vector<string> solve(RubiksCube cube) {
-        IDAStar ida;
-        vector<string> phase1 = ida.solve(cube, 8);
-        for(const auto& move : phase1) cube.applyMove(move);
-        vector<string> phase2 = ida.solve(cube, 14);
-        vector<string> solution = phase1;
-        solution.insert(solution.end(), phase2.begin(), phase2.end());
-        cout << "Kociemba Solution found!\nPhase 1: " << phase1.size() << "\nPhase 2: " << phase2.size() << "\nTotal: " << solution.size() << endl;
-        return solution;
-    }
-};
-
+// Main
 int main() {
-    RubiksCube cube;
-    vector<string> scramble = {"R", "U", "R'", "F", "R", "F'"};
+    initMoves();
+    Cube cube;
 
-    cout << "Applying scramble: ";
-    for (const string& move : scramble) {
-        cout << move << " ";
-        cube.applyMove(move);
+    vector<string> scramble = {"U", "R", "F", "D", "L", "B"};
+    for (auto& m : scramble) {
+        cube = apply(cube, moveTable[m]);
     }
-    cout << endl << endl;
 
-    cout << "=== Testing Algorithms ===" << endl << endl;
+    cout << "Solving scramble: ";
+    for (auto& m : scramble) cout << m << " ";
+    cout << endl;
 
-    IDAStar ida;
-    vector<string> idaSolution = ida.solve(cube);
-    cout << "IDA* Solution: ";
-    for (const string& move : idaSolution) cout << move << " ";
-    cout << endl << endl;
+    auto solution = idaStar(cube);
 
-    KociembaAlgorithm kociemba;
-    vector<string> kociembaSolution = kociemba.solve(cube);
-    cout << "Kociemba Solution: ";
-    for (const string& move : kociembaSolution) cout << move << " ";
-    cout << endl << endl;
+    cout << "Solution: ";
+    for (auto& move : solution) cout << move << " ";
+    cout << endl;
 
     return 0;
 }
